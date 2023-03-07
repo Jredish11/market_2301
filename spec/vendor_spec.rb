8fcf35f7baa1d6334775fcf006e3e4c707ff4bc3
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-07 09:54:49 -0700

    test: Add test for vendor class methods

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,14 +5,38 @@ RSpec.describe Vendor do
   before(:each) do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
-  describe '#initialize' do
-    it 'adds checks names and prices' do
+  describe 'item object' do
+    it 'checks names and prices' do
       expect(@item1.name).to eq('Peach')
       expect(@item2.name).to eq('Tomato')
       expect(@item2.price).to eq(0.5)
     end
+  end
+
+  describe '#initialize vendor' do
+    it 'initializes vendor class and has attributes' do
+      expect(@vendor.vendor_name).to eq('Rocky Mountain Fresh')
+      expect(@vendor.inventory).to eq({})
+    end
+
+    it 'checks if item is in stock, it adds items and counts values' do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+      
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.inventory).to eq({@item1 => 30})
+
+      expect(@vendor.check_stock(@item1)).to eq(30)
+
+      @vendor.stock(@item1, 25)
+
+      expect(@vendor.check_stock(@item1)).to eq(55)
+    end
+
+
   end
 
 
@@ -27,40 +51,17 @@ end
 
 
 
-#   There are **4** features in Iteration 1:
-# 1. Item Creation - including all attr_readers
-# 2. Vendor Creation - including all attr_readers
+#   There are **4** features in Iteration 1
 # 3. Vendor #check_stock
 # 4. Vendor #stock
 
 
 
 
-# pry(main)> item2.name
-# #=> "Tomato"
 
-# pry(main)> item2.price
-# #=> 0.5
 
-# pry(main)> vendor = Vendor.new("Rocky Mountain Fresh")
-# #=> #<Vendor:0x00007f85683152f0...>
 
-# pry(main)> vendor.name
-# #=> "Rocky Mountain Fresh"
 
-# pry(main)> vendor.inventory
-# #=> {}
-
-# pry(main)> vendor.check_stock(item1)
-# #=> 0
-
-# pry(main)> vendor.stock(item1, 30)
-
-# pry(main)> vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 30}
-
-# pry(main)> vendor.check_stock(item1)
-# #=> 30
 
 # pry(main)> vendor.stock(item1, 25)
 
