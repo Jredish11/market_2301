35667c3c0c89cad99dd58d671f14992996cedf91
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-07 11:55:31 -0700

    test for potential revenue

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -5,12 +5,75 @@ require './lib/market'
 RSpec.describe Market do
   before(:each) do
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")  
+    @vendor3 = Vendor.new("Palisade Peach Shack") 
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   describe "#initialize" do
     it 'creates a market and has attributes' do
       expect(@market.market_name).to eq("South Pearl Street Farmers Market")
       expect(@market.vendors).to eq([])
+    end
+  end
+
+  describe "add_vendor" do
+    it 'adds stock to vendor and then adds vendors to list' do
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65)
+
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    end
+
+    it 'checks vendor_names' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+
+    it 'makes a list of vendors that sell certain items' do
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65)
+
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+      expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+    end
+
+    it 'checks potential revenue for a vendor' do
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65)
+
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      @market.potential_revenue
+      expect(@vendor1.potential_revenue).to eq(29.75)
+      expect(@vendor2.potential_revenue).to eq(345.00)
+      expect(@vendor3.potential_revenue).to eq(48.75)
     end
   end
 
@@ -23,71 +86,12 @@ end
 
 
 
-# There are **5** features in Iteration 2:
-# 1. Market Creation - including all attr_readers
-# 2. Market #add_vendor
-# 3. Market #vendor_names
-# 4. Market #vendors_that_sell
+# There are **5** features in Iteration 2
+
 # 5. Vendor #potential_revenue
 
 
 
-
-
-# pry(main)> market.name
-# #=> "South Pearl Street Farmers Market"
-
-# pry(main)> market.vendors
-# #=> []
-
-# pry(main)> vendor1 = Vendor.new("Rocky Mountain Fresh")
-# #=> #<Vendor:0x00007fe1348a1160...>
-
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-
-# pry(main)> item2 = Item.new({name: 'Tomato', price: "$0.50"})
-# #=> #<Item:0x007f9c565c0ce8...>
-
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-
-# pry(main)> vendor1.stock(item1, 35)    
-
-# pry(main)> vendor1.stock(item2, 7)    
-
-# pry(main)> vendor2 = Vendor.new("Ba-Nom-a-Nom")    
-# #=> #<Vendor:0x00007fe1349bed40...>
-
-# pry(main)> vendor2.stock(item4, 50)    
-
-# pry(main)> vendor2.stock(item3, 25)
-
-# pry(main)> vendor3 = Vendor.new("Palisade Peach Shack")    
-# #=> #<Vendor:0x00007fe134910650...>
-
-# pry(main)> vendor3.stock(item1, 65)  
-
-# pry(main)> market.add_vendor(vendor1)    
-
-# pry(main)> market.add_vendor(vendor2)    
-
-# pry(main)> market.add_vendor(vendor3)
-
-# pry(main)> market.vendors
-# #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
-
-# pry(main)> market.vendor_names
-# #=> ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
-
-# pry(main)> market.vendors_that_sell(item1)
-# #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
-
-# pry(main)> market.vendors_that_sell(item4)
-# #=> [#<Vendor:0x00007fe1349bed40...>]
 
 # pry(main)> vendor1.potential_revenue
 # #=> 29.75
