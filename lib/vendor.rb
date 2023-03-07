aacdfc9ac4eff513d10a0f8c8f8aa394f1d4c354
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-07 09:54:29 -0700

    Add methods for vendor class, refactor check_stock method

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,4 +1,25 @@
 class Vendor
+    
+    attr_reader :vendor_name, :inventory, :stock_count
+  def initialize(vendor_name)
+    @vendor_name = vendor_name
+    @inventory = {}
+    @stock_count = 0
+  end
+
+  def check_stock(item)
+    if @inventory.include?(item)
+      @stock_count += @inventory[item]
+      @inventory[item]
+    else
+      0
+    end 
+  end
+
+  def stock(item, count)
+    inventory[item] = count
+    inventory 
+  end
 
  
   
